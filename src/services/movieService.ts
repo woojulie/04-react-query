40a7ed103327a16a4094105612dc0dfada805af8
Julie Pazushkina <162894528+woojulie@users.noreply.github.com>
2026-02-12 19:34:57 +0100

    upd

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,28 +1,21 @@
 import axios from "axios";
-import type { Movie } from "../types/movie";
+import type { MoviesResponse } from "../types/movie";
 
-const BASE_URL = "https://api.themoviedb.org/3/search/movie";
-
-const API_KEY = import.meta.env.VITE_TMDB_TOKEN;
-
-export interface MoviesResponse {
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-}
+const BASE_URL = "https://api.themoviedb.org/3";
 
 export const fetchMovies = async (
   query: string,
   page: number,
 ): Promise<MoviesResponse> => {
-  const response = await axios.get<MoviesResponse>(BASE_URL, {
+  const response = await axios.get<MoviesResponse>(`${BASE_URL}/search/movie`, {
     params: {
       query,
       page,
+      language: "en-US",
+      include_adult: false,
     },
     headers: {
-      Authorization: `Bearer ${API_KEY}`,
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
     },
   });
 
